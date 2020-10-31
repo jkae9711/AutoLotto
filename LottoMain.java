c34b99c648b5c574d1d9ffc3507f48b3a989d429
jkae9711 <67827532+jkae9711@users.noreply.github.com>
2020-10-31 22:00:30 +0900

    로또번호 추첨기1

---
+++ LottoMain.java
@@ -0,0 +1,22 @@
+import java.lang.Math;
+import java.util.ArrayList;
+
+public class LottoMain {
+    public static void main(String[] args) {
+        ArrayList<Integer> init = new ArrayList<>(); // Type: Integer, Integer 타입은 <>로 쓸 수 있다.
+        ArrayList<Integer> lotto_number_init = new ArrayList<>(); // Type: Integer
+
+        for(int i=0; i<10; i++) {
+            init.add(((int)(Math.random() * 45) + 1));
+        }
+
+        for(Integer item : init) {
+            if (!lotto_number_init.contains(item)) {
+                lotto_number_init.add(item)
+        }
+
+        for(int i=0; i<6; i++) {
+            System.out.print(lotto_number_init.get(i) + " ");
+        }
+    }
+}
